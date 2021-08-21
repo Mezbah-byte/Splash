2b6525b0ba35953f50931dd6d93e1d13a09e6a3d
Mezbah <softwarealko@gmail.com>
2021-08-21 12:39:30 +0600

    21/08/2021

---
+++ app/src/main/java/com/example/splash/introduction.java
@@ -0,0 +1,32 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class introduction extends AppCompatActivity {
+
+    ImageView logo,app_name,splashImg;
+    LottieAnimationView lottieAnimationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+
+
+        logo = findViewById(R.id.logo);
+        app_name = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.img);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        app_name.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+    }
+}
